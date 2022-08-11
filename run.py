82102b5cacbeeff3eb7795f537e103c3864db1e3
AsmaaAamir <asma.liaqat@outlook.com>
2022-08-11 01:56:10 +0000

    collct sals data for last 5 market in list column

--- run.py
+++ run.py
@@ -52,7 +52,7 @@ def validate_data(values):
         return False
 
     return True
-    
+
 
 def update_worksheet(data, worksheet):
     """
@@ -83,6 +83,21 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects collumns of data from sales worksheet, collecting 
+    the last enteries for each sandwiches returns th data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+   
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -95,4 +110,5 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+#main()
+sales_columns = get_last_5_entries_sales()
